c3310b4799ce4d9b09cbb580761817188fa1f467
Gustav Rasmussen <gcr84@hotmail.com>
2023-06-05 16:59:48 +0200

    chore: add css

--- workoutplans/workoutplans/settings.py
+++ workoutplans/workoutplans/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "workoutapp/static")
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
